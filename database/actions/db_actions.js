fab54fba5ccf69a62bc2d1599747cc81d731a595
badCompany55 <zachirvin45@gmail.com>
2019-04-06 11:18:27 -0500

    DB ACTIONS add db interations for: add user get all users get single user

---
+++ database/actions/db_actions.js
@@ -0,0 +1,21 @@
+const db = require("../dbConfig.js");
+
+module.exports = {
+  all_users,
+  single_user,
+  add_user
+};
+
+function all_users() {
+  return db("users");
+}
+
+function single_user(name) {
+  return db("users")
+    .where("username", name)
+    .first();
+}
+
+function add_user(user) {
+  db("users").insert(user);
+}
